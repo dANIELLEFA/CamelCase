e04f5d428f4a918ab873da34407b91c6c2ce5e01
OWNER <OWNER@FailorLT.example.org>
2019-09-03 22:20:17 -0600

    Thank you uncle rodger

--- camelCase/src/camelCase/camelCase.java
+++ camelCase/src/camelCase/camelCase.java
@@ -8,16 +8,30 @@ public class camelCase
 			{
 				Scanner userInput = new Scanner (System.in);
 				System.out.println("Enter two words with a space!");
-				String twoWords = userInput.nextLine();
+				String twoWords = userInput.nextLine().toLowerCase();
+				 
+				
+				// twoWords.split(" ");
 				int space = twoWords.indexOf(" ");
-				String firstWord = twoWords.substring(0, space).toLowerCase();
-				String firstLetter = twoWords.substring(space, space++).toUpperCase();
-				String endOfSecoundWord= twoWords.substring( space= space + 2).toUpperCase();
-				System.out.print(firstWord+firstLetter+endOfSecoundWord);
+				//twoWords.trim();
+				String firstWord = twoWords.substring(0,space);
+				String secondWord = twoWords.substring(space+2);
+				String firstLetterOfSecondWord = twoWords.substring(space+1, space+ 2).toUpperCase();
+				//String oneWord = twoWords.replace(" ","");
+				System.out.println(firstWord + firstLetterOfSecondWord + secondWord);
+				//      System.out.println(twoWords.substring(0, space) + twoWords.substring(space + 1, space + 2).toUpperCase() + twoWords.substring(space + 2));
+//			String firstWord = twoWords.substring(0,  space).toLowerCase();
+//				String firstLetter = twoWords..toUpperCase();
+//				String endOfSecoundWord= twoWords.substring( space ).toLowerCase();
+//				System.out.print(firstWord+firstLetter+endOfSecoundWord);
 				
-				//.replace(" ", "")
 				
-
+				}
+				
+			//.replace(" ", "")
+				
+// doNot
 			}
 
-	}
+
+	
